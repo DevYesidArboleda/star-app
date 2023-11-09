f7c634561d2ae2d2b2886a30affb3e982d41ca1a
DevYesidArboleda <yesison714@gmail.com>
2023-11-09 11:43:17 -0500

    tipar fetch

--- src/components/VideoStreaming.tsx
+++ src/components/VideoStreaming.tsx
@@ -1,18 +1,16 @@
 import React, { useState, useEffect } from "react";
 import ReactPlayer from "react-player";
 
-interface typeData {
-    doc:{
+interface typeData {    
       name:string;
       description:string;
       price:number;
-      tags:string[]|null;
-    }
+      tags:string[] ;    
 }
 
 export const VideoStreaming = () => {
   const [video, setVideo] = useState<JSX.Element | null>(null);
-  const [data, setData] = useState([]);
+  const [data, setData] = useState<typeData[]>([]);
   const [url, setUrl] = useState("");
 
   useEffect(() => {
@@ -24,8 +22,8 @@ export const VideoStreaming = () => {
         if (!response.ok) {
           throw new Error("Error al cargar los datos");
         }
-        const result:typeData = await response.json();
-        setData(result.doc[2]);
+        const result = await response.json();
+        setData(result.doc);
         setUrl(result.doc[2].videoUrl);
         console.log("respuesta", result);
         console.log("video", result.doc[2].videoUrl);
@@ -36,36 +34,52 @@ export const VideoStreaming = () => {
 
     fetchData();
   }, []);
+  
 
   useEffect(() => {
-    setVideo(<ReactPlayer url={`${url}`} controls={true} />);
+    setVideo(<ReactPlayer url={`${url}`} controls={true} height="750px" width="464px"/>);
   }, []);
 
   return (
     <div>
-      <div className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row ">
-        {video}
-        <div className="flex flex-col justify-between p-4 leading-normal">
-            <h1 className="text-4xl  text-[#274C77] font-bold">Producto</h1>
-            <br />
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-[#53545C] ">
-            {data.name}
-          </h5>
-          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            
-            Description: {data.description}
-          </p>
-          <br />
-          <h1 className="text-black">Tallas Disponibles</h1>
-          <ul className="flex gap-2">
-          {data.tags.map((items:any, index:number ) => {
-            return <li key={index} className="flex text-black">{items}</li>
-            })}
-          </ul>
-          <br />
-          <div className="p-6 pt-0">
-            <button
-              className="select-none rounded-lg bg-[#8B8C89] py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-slate-800/20 transition-all hover:shadow-lg hover:shadow-slate-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+      <div className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row pr-8">
+        <div className="w-auto h-4/5 rounded-md m-8 videoPlayer">
+          {video}
+        </div>
+        <div className="flex flex-col  h-full gap-12 justify-start items-start">
+          <div className="border-b-2 border-[#8B8C89] w-full">
+            <h1 className="text-2xl  text-[#53545C] font-bold">Producto</h1>
+              <br />
+            <h5 className="mb-2 text-xl font-normal tracking-tight text-black ">
+              { data && data[2]?.name }
+            </h5>
+          </div>
+          <div className="border-b-2 border-[#8B8C89] w-full">
+            <p className="mb-3 font-normal text-black text-xl">            
+              Pago contra entrega
+            </p>
+          </div>
+          <div className="border-b-2 border-[#8B8C89] w-full">
+            <p className="mb-3 font-bold text-2xl text-[#53545C] ">            
+              Envío gratis
+            </p>
+          </div>
+          <div className="border-b-2 border-[#8B8C89] w-full">
+            <p className="mb-2 text-xl font-normal tracking-tight text-black">            
+              Description: { data && data[2]?.description }
+            </p>
+          </div>
+          <div className="border-b-2 border-[#8B8C89] w-full">
+            <h1 className="mb-3 font-bold text-2xl text-[#53545C]">Variaciones</h1>
+              <ul className="flex gap-2">
+              {data.length > 0 && data[2]?.tags.map((items:any, index:number ) => {
+                return <li key={index} className="mb-2 text-xl font-normal tracking-tight text-black">{items}</li>
+                }) }
+              </ul>
+          </div>
+          <div className="flex w-full justify-center">
+          <button
+              className="select-none rounded-lg bg-[#8B8C89] py-3 px-12 normal-case text-center align-middle font-sans text-xl font-normal text-white shadow-md shadow-slate-800/20 transition-all hover:shadow-lg hover:shadow-slate-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
               type="button"
               data-ripple-light="true"
             >
@@ -77,3 +91,16 @@ export const VideoStreaming = () => {
     </div>
   );
 };
+
+/*
+  Traer todos los Productos
+
+    <div>
+    { data.map((item:typeData, index:number) => {
+    console.log("item:::::", item)
+    return (
+        <div key={index}>{item.name}</div>
+    )
+  })}
+  </div>
+*/
