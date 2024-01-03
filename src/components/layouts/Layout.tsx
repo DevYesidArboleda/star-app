2e35504eaa67bc3188bcb1db490e75497f6a0a0f
DevYesidArboleda <yesison714@gmail.com>
2024-01-03 10:44:24 -0500

    Ajuste de thumbnail

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -16,17 +16,19 @@ const origin = (typeof window === 'undefined') ? '' : window.location.origin;
 
 
 export const Layout: FC<Props> = ({ children, title, thumbnail, name }) => {
+
+  console.log(`"${ title }"`)
   
   return (
       <>        
           <Head>
             <title>{ title || 'Checkout' }</title>
             <meta name="author" content="Dropi" />
-            <meta name="description" content={`Checkout de ordenes ${ title }`} />
-            <meta name="keywords" content={ `${ title }`} />
-            <meta property="og:title" content={`${name}`} />
+            <meta name="description" content={`"Checkout de ordenes ${ title }"`} />
+            <meta name="keywords" content={ `"${ title }"`} />
+            <meta property="og:title" content={`"${name}"`} />
             <meta property="og:description" content="Checkout para procesar la orden de tus productos" />
-            <meta property="og:image" content={`${thumbnail}`} />
+            <meta property="og:image" content={`"${thumbnail}"`} />
 
         </Head>        
       
