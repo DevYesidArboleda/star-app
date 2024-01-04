95a4ae6fa89faffef486027dfc8b44df713e8562
DevYesidArboleda <yesison714@gmail.com>
2024-01-04 17:37:13 -0500

    Ajuste final thumbnail

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -18,17 +18,7 @@ const origin = (typeof window === 'undefined') ? '' : window.location.origin;
 export const Layout: FC<Props> = ({ children, title, thumbnail, name }) => {
   
   return (
-      <>        
-          <Head>
-            <title>{ title || 'Checkout' }</title>
-            <meta name="author" content="Dropi" />
-            <meta name="description" content={`"Checkout de ordenes ${ title }"`} />
-            <meta name="keywords" content={ `"${ title }"`} />
-            <meta property="og:title" content={`"${name}"`} />
-            <meta property="og:description" content="Checkout para procesar la orden de tus productos" />
-            <meta property="og:image" content={`"${thumbnail}"`} />
-        </Head>   
-
+      <>   
         <NextSeo
         title="Checkout Estrellas"
         description="Checkout para procesar la orden de tus productos"
