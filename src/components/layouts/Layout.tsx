d2ba66e4aefa5c8f502b6cc2f5384a0197463df3
DevYesidArboleda <yesison714@gmail.com>
2024-01-05 11:25:02 -0500

    Ajuste head

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -20,23 +20,16 @@ export const Layout: FC<Props> = ({ children, title, thumbnail, name }) => {
   return (
       <>                  
 
-        <NextSeo
-        title="Checkout Estrellas"
-        description="Checkout para procesar la orden de tus productos"
-        openGraph={{
-          title: `${name}`,
-          description: 'Checkout para procesar la orden de tus productos',
-          url: 'https://star-app-tau.vercel.app/',
-          images: [
-            {
-              url: `${thumbnail}`,
-              width: 1200,
-              height: 630,
-              alt: 'Texto alternativo de la imagen',
-            },
-          ],
-        }}
-      />     
+<Head>
+            <title>{ title || 'Checkout' }</title>
+            <meta name="author" content="Dropi" />
+            <meta name="description" content={`Checkout de ordenes ${ title }`} />
+            <meta name="keywords" content={ `${ title }`} />
+            <meta property="og:title" content="Informacion del producto" />
+            <meta property="og:description" content="Checkout para procesar la orden de tus productos" />
+            <meta property="og:image" content={`${thumbnail}`} />
+
+        </Head> 
       
         <NavBar />
 
