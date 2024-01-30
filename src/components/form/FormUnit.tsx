65a36854ffab3a28028b776523f6e3c174c72954
DevYesidArboleda <yesison714@gmail.com>
2024-01-30 16:35:02 -0500

    Agregar y borrar productos

--- src/components/form/FormUnit.tsx
+++ src/components/form/FormUnit.tsx
@@ -49,15 +49,6 @@ interface typeData {
   tags: string[];
 }
 
-const people = [
-  { id: 1, name: "Wade Cooper" },
-  { id: 2, name: "Arlene Mccoy" },
-  { id: 3, name: "Devon Webb" },
-  { id: 4, name: "Tom Cook" },
-  { id: 5, name: "Tanya Fox" },
-  { id: 6, name: "Hellen Schmidt" },
-];
-
 export default function Form(dataFinal: any) {
   const { previousStep, setPreviousStep } = usePrevs();
   const { currentStep, setCurrentStep } = useSteps();
--- src/components/videoComponents/VideoList.tsx
+++ src/components/videoComponents/VideoList.tsx
@@ -1,25 +1,78 @@
-import { useEffect, useState } from 'react'
-import { useGetFeedVideos } from '../../hooks/useGetFeedVideos'
-import Video from './Video'
+import { useEffect, useState } from "react";
+import { useGetFeedVideos } from "../../hooks/useGetFeedVideos";
+import Video from "./Video";
+import Image from "next/image";
 
-export default function VideoList () {
-  const [videos, setVideos] = useState()
+export default function VideoList() {
+  const [videos, setVideos] = useState<any>();
+  const [posicionActual, setPosicionActual] = useState<number | null>(null);
+  const [posicionesCapturadas, setPosicionesCapturadas] = useState<number[]>(
+    []
+  );
+
+  const handleBotonClick = (index: number) => {
+    setPosicionActual(index);
+
+    if (videos && videos.length > 0 && index < videos.length) {
+      const videoSeleccionado: any = videos[index];
+      console.log("Video seleccionado:", videoSeleccionado);
+      setPosicionesCapturadas((prevPosiciones: any) => [
+        ...prevPosiciones,
+        videoSeleccionado.externalId,
+      ]);
+    } else {
+      console.error("La posición actual no es válida");
+    }
+  };
+
+  const handleBorrarClick = (index: number) => {
+    setPosicionesCapturadas((prevPosiciones) =>
+      prevPosiciones.filter((pos) => pos !== videos[index].externalId)
+    );
+  };
+
+  console.log(posicionesCapturadas);
 
   useEffect(() => {
     // eslint-disable-next-line react-hooks/rules-of-hooks
-    useGetFeedVideos()
-      .then(videos => setVideos(videos))
-  }, [])
+    useGetFeedVideos().then((videos) => setVideos(videos));
+  }, []);
 
   return (
-    <section className='pb-14 h-full absolute top-0 right-0 left-0 flex flex-col snap-y snap-mandatory overflow-y-auto'>
+    <section className="pb-14 h-full absolute top-0 right-0 left-0 flex flex-col snap-y snap-mandatory overflow-y-auto">
       {!videos
-        ? 'Loading...'
-        // @ts-ignore
-        : videos.map((video, index) => {
-          return <Video key={index} src={video.videoUrl} />
-        })
-      }
+        ? "Loading..."
+        : // @ts-ignore
+          videos.map((video, index) => {
+            return (
+              <div className="flex flex-row-reverse items-center" key={index}>
+                <Video src={video.videoUrl} />
+                <button
+                  onClick={() => handleBotonClick(index)}
+                  className="text-white absolute mb-14"
+                >
+                  <Image src="/img/mas.png" alt="" width={32} height={32} />
+                </button>
+                <button
+                  onClick={() => handleBorrarClick(index)}
+                  className="text-white absolute mt-5"
+                >
+                  <Image src="/img/borrar.png" alt="" width={32} height={32} />
+                </button>
+                {posicionesCapturadas.length !== 0 ? (
+                  <button
+                    className=" fixed bottom-0 btn-success w-full h-[54px] text-base"
+                    type="button"
+                    data-ripple-light="true"
+                  >
+                    Finalizar Compra
+                  </button>
+                ) : (
+                  ""
+                )}
+              </div>
+            );
+          })}
     </section>
-  )
+  );
 }
